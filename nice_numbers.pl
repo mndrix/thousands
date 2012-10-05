2cf0337230965bb480c374d9eed10e176056df04
Michael Hendricks <michael@ndrix.org>
2012-10-05 17:54:41 -0600

    First draft of n macros for easier numbers
    It supports n(1,000) and n(2,123,456), etc.  It also supports
    arithmetic expressions inside like n(2*7 + 4).

---
+++ nice_numbers.pl
@@ -0,0 +1,57 @@
+:- module(nice_numbers, []).
+:- use_module(library(apply), [maplist/3]).
+
+expand_n(n(Thousands, Ones), X) :-
+    integer(Thousands),
+    integer(Ones),
+    X is 1000*Thousands + Ones.
+
+expand_n(n(Millions, Thousands, Ones), X) :-
+    integer(Millions),
+    integer(Thousands),
+    integer(Ones),
+    X is 1000000*Millions + 1000*Thousands + Ones.
+
+expand_n(n(Billions, Millions, Thousands, Ones), X) :-
+    integer(Billions),
+    integer(Millions),
+    integer(Thousands),
+    integer(Ones),
+    X is 1000000000*Billions + 1000000*Millions + 1000*Thousands + Ones.
+
+
+goal_expansion_loop(T, T) :-
+    var(T),
+    !.
+goal_expansion_loop(T0, T) :-
+    T0 =.. [Functor|Args0],
+    ( Args0=[] -> T = T0
+    ; Functor=n -> expand_n(T0, T)
+    ; maplist(goal_expansion_loop, Args0, Args),
+        T =.. [Functor|Args]
+    ).
+
+% expand n/{2,...} macros
+user:goal_expansion(T0, T) :-
+    nice_numbers:goal_expansion_loop(T0, T).
+
+
+goal_expansion_math(_, T, T) :-
+    var(T),
+    !.
+goal_expansion_math(T is Expression, n(Expression), T) :- !.
+goal_expansion_math(Math, T0, T) :-
+    T0 =.. [Functor|Args0],
+    (   Args0=[] -> T=T0
+    ;   maplist(goal_expansion_math(Math), Args0, Args),
+        T =.. [Functor|Args]
+    ).
+
+
+% expand n/1 arithmetic macros
+user:goal_expansion(T0, (Math,T)) :-
+    %format('GOAL: ~w~n', [T0]),
+    \+ (_,_) = T0,  % skip conjunction of smaller goals
+    \+ (_;_) = T0,  % skip disjunctions of smaller goals
+    nice_numbers:goal_expansion_math(Math, T0, T),
+    nonvar(Math).
---
+++ test.pl
@@ -0,0 +1,15 @@
+:- use_module(nice_numbers).
+
+main :-
+    format('Thousands: ~D~n', [n(4,321)]),
+    X = n(9,876,543),
+    (   format('Millions: ~D~n', [X])
+    ;   format('Math: ~d~n', [n(10*X)])
+    ).
+
+fib(0, 1) :- !.
+fib(1, 1) :- !.
+fib(N, FibN) :-
+    fib(n(N-1), N1),
+    fib(n(N-2), N2),
+    FibN is N1 + N2.
