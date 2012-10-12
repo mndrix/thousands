adc2986230691fbbbb9b1115cdb8731539d076ec
Michael Hendricks <michael@ndrix.org>
2012-10-12 12:19:17 -0600

    Correct download location URL

--- pack.pl
+++ pack.pl
@@ -6,4 +6,4 @@ author( 'Michael Hendricks', 'michael@ndrix.org' ).
 packager( 'Michael Hendricks', 'michael@ndrix.org' ).
 maintainer( 'Michael Hendricks', 'michael@ndrix.org' ).
 home('https://github.com/mndrix/thousands').
-download('https://github.com/downloads/mndrix/thousands/thousands-*.tgz').
+download('https://github.com/mndrix/thousands/downloads/thousands-*.tgz').
