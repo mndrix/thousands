53c68469928a9119240d4389eeb4a7f66ccf5350
Michael Hendricks <michael@ndrix.org>
2012-10-12 12:15:29 -0600

    Include download location

--- pack.pl
+++ pack.pl
@@ -6,3 +6,4 @@ author( 'Michael Hendricks', 'michael@ndrix.org' ).
 packager( 'Michael Hendricks', 'michael@ndrix.org' ).
 maintainer( 'Michael Hendricks', 'michael@ndrix.org' ).
 home('https://github.com/mndrix/thousands').
+download('https://github.com/downloads/mndrix/thousands/thousands-*.tgz').
