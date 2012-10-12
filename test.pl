dd4a1e3274f4fa23d12636592fbcefed4c8b797d
Michael Hendricks <michael@ndrix.org>
2012-10-12 11:51:55 -0600

    Include an example with billions

--- test.pl
+++ test.pl
@@ -3,4 +3,5 @@
 main :-
     format('Thousands: ~D~n', [n(4,321)]),
     X = n(9,876,543),
-    format('Millions: ~D~n', [X]).
+    format('Millions: ~D~n', [X]),
+    format('32 years in seconds: ~D~n', [n(1,009,152,000)]).
