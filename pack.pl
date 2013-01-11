9a41e69b2f483151d838de1077c09a5793ba5d6e
Michael Hendricks <michael@ndrix.org>
2013-01-11 14:13:19 -0700

    Bump version to circumvent caching

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(thousands).
-version('0.1.0').
+version('0.1.1').
 title('Thousands separator for large integers').
 keywords([macro,integers]).
 author( 'Michael Hendricks', 'michael@ndrix.org' ).
 packager( 'Michael Hendricks', 'michael@ndrix.org' ).
 maintainer( 'Michael Hendricks', 'michael@ndrix.org' ).
 home('http://packs.ndrix.com/thousands/index.html').
-download('http://packs.ndrix.com/thousands/thousands-0.1.0.tgz').
+download('http://packs.ndrix.com/thousands/thousands-0.1.1.tgz').
