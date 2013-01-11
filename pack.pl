413d6f7108e461ec44a693a0c23c39b1de40e34d
Michael Hendricks <michael@ndrix.org>
2013-01-11 13:46:39 -0700

    Bump version number

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(thousands).
-version('0.0.2').
+version('0.1.0').
 title('Thousands separator for large integers').
 keywords([macro,integers]).
 author( 'Michael Hendricks', 'michael@ndrix.org' ).
 packager( 'Michael Hendricks', 'michael@ndrix.org' ).
 maintainer( 'Michael Hendricks', 'michael@ndrix.org' ).
 home('http://commondatastorage.googleapis.com/ndrix/thousands/index.html').
-download('http://commondatastorage.googleapis.com/ndrix/thousands-0.0.2.tgz').
+download('http://commondatastorage.googleapis.com/ndrix/thousands-0.1.0.tgz').
