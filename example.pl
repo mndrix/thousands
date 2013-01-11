4b68f6b5f2daa642ada11453a72f144b168b21d1
Michael Hendricks <michael@ndrix.org>
2013-01-11 12:35:11 -0700

    Make example.pl executable

--- example.pl
+++ example.pl
@@ -1,6 +1,7 @@
+#!/usr/bin/env swipl -q -g main -s
 :- use_module(thousands).
 
-main :-
+main(_) :-
     format('Thousands: ~D~n', [n(4,321)]),
     X = n(9,876,543),
     format('Millions: ~D~n', [X]),
