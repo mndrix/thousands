b09c6e24b4d0b97c9f1a4d4395ae30aad16f280e
Michael Hendricks <michael@ndrix.org>
2013-01-18 15:45:32 -0700

    Re-implement with function_expansion module
    Let that module handle all the gritty details about rewriting terms.
    This module can focus on the simple goal of multiplying a few decimal
    places.

--- pack.pl
+++ pack.pl
@@ -1,9 +1,10 @@
 name(thousands).
-version('0.1.1').
+version('0.2.0').
 title('Thousands separator for large integers').
+requires(function_expansion).
 keywords([macro,integers]).
 author( 'Michael Hendricks', 'michael@ndrix.org' ).
 packager( 'Michael Hendricks', 'michael@ndrix.org' ).
 maintainer( 'Michael Hendricks', 'michael@ndrix.org' ).
 home('http://packs.ndrix.com/thousands/index.html').
-download('http://packs.ndrix.com/thousands/thousands-0.1.1.tgz').
+download('http://packs.ndrix.com/thousands/thousands-0.2.0.tgz').
--- prolog/thousands.pl
+++ prolog/thousands.pl
@@ -1,5 +1,5 @@
 :- module(thousands, [n/2,n/3,n/4]).
-:- use_module(library(apply), [maplist/3]).
+:- use_module(library(function_expansion)).
 
 % The following definitions for n/{2,3,4} are strictly for the
 % documentation system.  These macros are expanded away.
@@ -19,38 +19,21 @@ n(_,_,_).
 %	Represents an integer less than 1 trillion.
 n(_,_,_,_).
 
-expand_n(n(Thousands, Ones), X) :-
-    expand_n(n(0, 0, Thousands, Ones), X).
+user:function_expansion(n(Thousands,Ones), X, true) :-
+    calc_n(0, 0, Thousands, Ones, X).
 
-expand_n(n(Millions, Thousands, Ones), X) :-
-    expand_n(n(0, Millions, Thousands, Ones), X).
+user:function_expansion(n(Millions,Thousands,Ones), X, true) :-
+    calc_n(0, Millions, Thousands, Ones, X).
 
-expand_n(n(Billions, Millions, Thousands, Ones), X) :-
+user:function_expansion(n(Billions,Millions,Thousands,Ones), X, true) :-
+    calc_n(Billions, Millions, Thousands, Ones, X).
+
+calc_n(Billions, Millions, Thousands, Ones, X) :-
     integer(Billions),
     integer(Millions),
     integer(Thousands),
     integer(Ones),
     X is 1000000000*Billions + 1000000*Millions + 1000*Thousands + Ones.
-
-
-goal_expansion_loop(T, T) :-  % so `X=n(1,234)` works
-    var(T),
-    !.
-goal_expansion_loop(T0, T) :-
-    expand_n(T0, T),  % integer/1 constraints imply nonvar(T0)
-    !.
-goal_expansion_loop(T0, T) :-  % look for expandable terms inside T0
-    nonvar(T0),
-    \+ expand_n(T0, _),
-    T0 =.. [Functor|Args0],
-    maplist(goal_expansion_loop, Args0, Args),
-    T =.. [Functor|Args].
-
-% expand n/{2,3,4} macros
-user:goal_expansion(T0, T) :-
-    (_,_) \= T0,  % ignore conjunction of smaller goals
-    (_;_) \= T0,  % ignore disjunction of smaller goals
-    thousands:goal_expansion_loop(T0, T).
 
 :- begin_tests(thousands).
 test(billions) :-
